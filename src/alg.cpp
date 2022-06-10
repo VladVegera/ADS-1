778a9c1ba2e8323264214c0c3a6b4dd3aacaf4c9
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 22:11:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -45,8 +45,8 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for(int i = 2 ; i < hbound; i++) {
-    if(checkPrime(i)) {
+  for (int i = 2 ; i < hbound; i++) {
+    if (checkPrime(i)) {
       sum += i;
     }
   }
