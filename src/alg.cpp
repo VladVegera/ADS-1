79ee759bbb8838ce087f818b9eab446de2670ed5
VladVegera <94718115+VladVegera@users.noreply.github.com>
2022-06-10 22:09:20 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,50 +3,52 @@
 #include "alg.h"
 
 bool checkPrime(uint64_t value) {
-if (value == 0 || value == 1) {
-  return false;
-}
-if (value == 2) {
-  return true;
-}
-for (int i = 2; i <= value / 2; i++) {
-  if (value % 2 == 0) {
-   return false;
+  if (value == 0 || value == 1) {
+    return false;
   }
-}
-return true;
+  if (value == 2) {
+    return true;
+  }
+  for (int i = 2; i <= value / 2; i++) {
+    if (value % 2 == 0) {
+     return false;
+    }
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-if (n == 1) {
-  return 2;
-}
-uint64_t count = 1;
-for(uint64_t i = 3; ; i+=2) {
-  if(checkPrime(i)) {
-    count++;
+  bool f = true;
+  uint64_t x = 0, num = 0;
+  while (f) {
+    if (checkPrime(num)) {
+      ++x;
+    }
+    if (m == n) {
+      break;
+    }
+    ++num;
   }
-  if(count == n) {
-    return i;
-  }
-}
+  return num;
 }
 
 uint64_t nextPrime(uint64_t value) {
-int next = value+1;
-if(checkPrime(next)) {
-  return next;
-} else {
-  next++;
-}
+  bool f = true;
+  uint64_t temp = 0;
+  for (temp = value + 1; f; ++temp) {
+    if (checkPrime(temp)) {
+      break;
+    }
+  }
+  return temp;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-uint64_t sum = 0;
-for(int i = 2 ; i < hbound; i++) {
-  if(checkPrime(i)) {
-    sum += i;
+  uint64_t sum = 0;
+  for(int i = 2 ; i < hbound; i++) {
+    if(checkPrime(i)) {
+      sum += i;
+    }
   }
-}
-return sum;
+  return sum;
 }
